4e9dd2f460eac730bddebfa31361bd13bfcc4c92
galaida <galaida.arseniy@gmail.com>
2023-07-20 21:59:41 +0300

    comleted

--- hw_26/classwork/script_posts.js
+++ hw_26/classwork/script_posts.js
@@ -40,7 +40,7 @@ class PostPage {
     this.inputsForm = new myDiv(settings)
     inputsDiv.appendChild(this.inputsForm.htmlDiv)
 
-    this.postsDiv = document.createElement('div') // div for displaying posts
+    this.postsDiv = document.createElement('div') // div for displaying postss
     this.createHTML()
     document.body.appendChild(inputsDiv)
     document.body.appendChild(this.postsDiv)
@@ -70,10 +70,8 @@ class PostPage {
       if (inputTitle.value !== '' && inputBody.value !== '') {
         let body = {'id': this.posts.length + 1}
         this.inputsForm.inputs.forEach(input => {
-          console.log(input.name)
           body[input.name] = input.value
           if (!input.readOnly) {
-            console.log(body)
             input.value = ""
           }
         })
@@ -86,11 +84,9 @@ class PostPage {
         })
           .then(response => response.json())
           .then(postData => {
-            console.log(postData)
             let post = new Post(postData)
             this.postsDiv.appendChild(post.htmlPost)
             this.posts.push(post)
-            console.log(this.posts)
           })
       }
     })
@@ -104,35 +100,45 @@ class Post {
     this.title = title
     this.body = body
     this.htmlPost = this.renderPost() // here is div for current post
+    this.htmlPost.style.textAlign = 'center'
   }
 
   renderPost() {
     let div = document.createElement('div')
-    div.style.backgroundColor = 'black'
-    div.style.color = 'white'
-    div.style.marginBottom = '10px'
+    div.style.backgroundColor = 'rgba(0, 0, 0, 0.12)'
+    div.style.borderRadius = '36px'
+    div.style.textAlign = 'center'
     let postHeader = document.createElement('h1')
     postHeader.innerText = 'POST'
-    postHeader.style.color = 'white'
-    postHeader.style.backgroundColor = 'black'
     postHeader.style.marginBottom = '0'
     postHeader.style.marginTop = '36px'
+    let commentsHeader = document.createElement('h2')
+    commentsHeader.innerText = 'COMMENTS'
+    commentsHeader.style.marginBottom = '0'
+    commentsHeader.style.marginTop = '36px'
     let title = document.createElement('p')
     title.innerText = this.title
     let body = document.createElement('p')
     body.innerText = this.body
-    let buttonsDiv = document.createElement('div')
+    let activeDiv = document.createElement('div')
     let changeBtn = document.createElement('button')
     changeBtn.innerText = 'Change'
+    changeBtn.className = 'btn btn-info'
+    changeBtn.style.padding = '2px 4px 2px 4px'
+    changeBtn.style.marginRight = '2px'
     let deleteBtn = document.createElement('button')
     deleteBtn.innerText = 'Delete'
-    buttonsDiv.appendChild(changeBtn)
-    buttonsDiv.appendChild(deleteBtn)
+    deleteBtn.className = 'btn btn-warning'
+    deleteBtn.style.padding = '2px 6px 2px 6px'
+    activeDiv.appendChild(changeBtn)
+    activeDiv.appendChild(deleteBtn)
     div.appendChild(postHeader)
     div.appendChild(title)
     div.appendChild(body)
-    div.appendChild(buttonsDiv)
+    div.appendChild(activeDiv)
+    div.appendChild(commentsHeader)
 
+    let commentTextDiv = document.createElement('div')
     fetch(`https://jsonplaceholder.typicode.com/posts/${this.id}/comments`, {
       method: 'GET',
       headers: {
@@ -141,68 +147,71 @@ class Post {
     })
       .then(response => response.json())
       .then(comments => {
-        comments.forEach(comment => {
-          let newComment = new Comment(comment.postId, comment.id, comment.name, comment.email, comment.body)
+        comments.forEach((comment, index) => {
+          let newComment = new Comment(comment, index + 1)
 
-          let commentHeader = document.createElement('h2')
-          commentHeader.innerText = 'COMMENT'
-
-          div.appendChild(commentHeader)
-          div.appendChild(newComment.htmlComment)
+          commentTextDiv.appendChild(newComment.htmlComment)
         })
       })
+    div.appendChild(commentTextDiv)
 
     const data = [this.userId, this.id, this.title, this.body]
-    const deleteButton = document.getElementById('delete')
-    deleteBtn.addEventListener('click', () => {
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'DELETE',
-        body: JSON.stringify({data}),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(() => div.innerText = '')
-    })
 
     changeBtn.addEventListener('click', () => {
+      let inputsDiv = document.createElement('div')
       let inputTitle = document.createElement('input')
       inputTitle.value = data[2]
-      inputTitle.style.width = '1000px'
+      inputTitle.className = 'form-control'
+      inputTitle.style.marginTop = '6px'
       let inputBody = document.createElement('input')
       inputBody.value = data[3]
-      inputBody.style.width = '1000px'
-      buttonsDiv.innerText = ''
-      buttonsDiv.appendChild(inputTitle)
-      buttonsDiv.appendChild(inputBody)
+      inputBody.className = 'form-control'
+      inputBody.style.marginTop = '6px'
+      activeDiv.innerText = ''
+      inputsDiv.appendChild(inputTitle)
+      inputsDiv.appendChild(inputBody)
       let saveBtn = document.createElement('button')
       saveBtn.innerText = 'Save'
-      saveBtn.style.marginLeft = '20px'
-      buttonsDiv.appendChild(saveBtn)
+      saveBtn.innerText = 'save'
+      saveBtn.className = 'btn btn-success'
+      saveBtn.style.padding = '2px 6px 2px 6px'
+      saveBtn.style.marginTop = '6px'
+      activeDiv.style.alignItems = 'center'
+      activeDiv.appendChild(inputsDiv)
+      activeDiv.appendChild(saveBtn)
+      activeDiv.style.alignItems = 'center'
+
       saveBtn.addEventListener('click', () => {
         data[2] = inputTitle.value
         data[3] = inputBody.value
+        console.log({title: data[2], body: data[3]})
         fetch('https://jsonplaceholder.typicode.com/posts', {
           method: 'PATCH',
-          body: JSON.stringify({data}),
+          body: JSON.stringify({title: data[2], body: data[3]}),
           headers: {
             'Content-Type': 'application/json'
           }
         })
           .then(response => response.json())
           .then(newData => {
-            console.log(newData)
-            title = newData.title
-            body = newData.body
-            buttonsDiv.innerText = ''
-            let changeBtn = document.createElement('button')
-            changeBtn.innerText = 'Change'
-            let deleteBtn = document.createElement('button')
-            deleteBtn.innerText = 'Delete'
-            buttonsDiv.appendChild(changeBtn)
-            buttonsDiv.appendChild(deleteBtn)
+            title.innerText = newData.title || data[2]
+            body.innerText = newData.body || data[3]
+
+            activeDiv.innerText = ''
+            activeDiv.appendChild(changeBtn)
+            activeDiv.appendChild(deleteBtn)
           })
+        .catch(err => {
+          console.log("there is an error")
+        });
       })
+    })
+
+    deleteBtn.addEventListener('click', () => {
+      fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'DELETE',
+      })
+        .then(() => div.innerText = '')
     })
     return div
   }
@@ -210,30 +219,34 @@ class Post {
 
 
 class Comment {
-  constructor(postId, id, name, email, body) {
+  constructor({postId, id, name, email, body}, numberOfComment) {
     this.postId = postId
     this.id = id
     this.name = name
     this.email = email
     this.body = body
+    this.numberOfComment = numberOfComment
     this.htmlComment = this.renderComments() // here is div for current comment
   }
 
   renderComments() {
     let div = document.createElement('div')
-    div.style.backgroundColor = 'black'
-    div.style.color = 'white'
     div.style.marginBottom = '10px'
+    let commentHeader = document.createElement('p')
+    commentHeader.style.fontWeight = '700'
+    commentHeader.innerText = `${this.numberOfComment}`
     let name = document.createElement('p')
     name.innerText = this.name
     let email = document.createElement('p')
     email.innerText = this.email
+    email.style.fontWeight = '500'
     let body = document.createElement('p')
     body.innerText = this.body
 
+    div.appendChild(commentHeader)
     div.appendChild(name)
-    div.appendChild(email)
     div.appendChild(body)
+    div.appendChild(email)
     return div
   }
 }
@@ -255,9 +268,10 @@ class myDiv {
     let header = document.createElement('h1')
     header.style.color = 'white'
     header.innerText = 'Post form'
+    header.style.color = 'Black'
     header.style.marginBottom = '16px'
     this.htmlDiv.appendChild(header)
-    document.body.style.backgroundColor = '#035565'
+    // document.body.style.backgroundColor = '#035565'
   }
 
   addInput(params) {
@@ -265,10 +279,12 @@ class myDiv {
     params.map(item => {
       input.setAttribute(item.key, item.value)
     })
+    input.style.borderWidth = '1px'
+    input.style.borderColor = 'black'
     let p = document.createElement('p')
     p.style.marginLeft = '6px'
     p.style.marginTop = '6px'
-    p.style.color = 'yellow'
+    p.style.color = 'red'
     input.addEventListener('focus', (event) => {
       if (input.required && input.value === '') {
           p.innerText = `input name: ${input.name} can't be empty`
@@ -283,29 +299,6 @@ class myDiv {
           p.innerText = ''
       }
     })
-    // this.btn.addEventListener("click", () => {
-    //   let body = {}
-    //   this.inputs.forEach(input => {
-    //     body[input.name] = input.value
-    //     if (!input.readOnly) {
-    //       console.log(body)
-    //       input.value = ""
-    //     }
-    //   })
-    //   fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: 'POST',
-    //     body: JSON.stringify(body),
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    //       .then(response => response.json())
-    //       .then(function (postData) {
-    //         let post = new Post(postData)
-    //         htmlPostsBlock.appendChild(post.htmlPost)
-    //         postPage.posts.push(post)
-    //       })
-    //   })
-    // })
     this.htmlDiv.appendChild(input)
     this.inputs.push(input)
     this.htmlDiv.appendChild(p)
@@ -314,7 +307,7 @@ class myDiv {
   addBtn() {
     this.btn.type = "submit"
     this.btn.id = "submit"
-    this.btn.className = "btn btn-secondary btn-sm"
+    this.btn.className = "btn btn-success btn-sm"
     this.btn.innerText = "Submit"
     let div = document.createElement("div")
     div.appendChild(this.btn)
